a3d89d591d369bbc1bb116700cdcd0629b729ea4
Sumit Khandelwal <synergy2411@outlook.com>
2023-10-09 14:09:45 +0530

    initial commit

---
+++ 01-basics/app.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function () {
+
+    // $("em").parents().addClass("border")
+    // $(".item-1").parentsUntil(".container-two").addClass("border")
+
+
+    // $("#drinks").find("li:first").siblings().css({ color: "red" })
+
+
+    // $("#drinks").find("li:first").next().next().prev().css({ color: "green" })
+
+
+
+});
+
