603854dd73b6092f348894c423432d7ba26ad15b
Sumit Khandelwal <synergy2411@outlook.com>
2023-10-09 14:57:01 +0530

    exercise added

--- 01-basics/app.js
+++ 01-basics/app.js
@@ -10,6 +10,29 @@ jQuery(document).ready(function () {
     // $("#drinks").find("li:first").next().next().prev().css({ color: "green" })
 
 
+    // $("#drinks li").first().addClass("underline")
+
+    // $("#drinks li").eq(1).addClass("underline")
+
+    // $("#drinks li").has("a").addClass("border")
+
+    // $("#drinks li").slice(0, 2).addClass("border")
+
+    // $("#drinks li").not(".item-1").addClass("border")
+
+    $("#btnFetch").on("click", function () {
+        // $(".header-container").load("./templates/header.html #second")
+
+        $.get("http://localhost:3000/todos", function (data, statusText, xhr) {
+            console.log(data);
+        })
+
+        $.post("http://localhost:3000/todos", { title: "...", status: "pending" }, function (data, statusText, xhr) {
+            console.log(data, statusText, xhr);
+        })
+
+    })
+
 
 });
 
