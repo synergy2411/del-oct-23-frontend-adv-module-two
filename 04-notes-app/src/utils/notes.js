1f89defc57fbb36c841fe280fd8113a27b94a470
Sumit Khandelwal <synergy2411@outlook.com>
2023-10-11 13:01:08 +0530

    git presentation added

--- 04-notes-app/src/utils/notes.js
+++ 04-notes-app/src/utils/notes.js
@@ -2,25 +2,35 @@ const fs = require("fs");
 require("colors");
 
 const loadNotes = () => {
-    try {
-        const notesBuffer = fs.readFileSync("./data.json")
-        return JSON.parse(notesBuffer.toString())
-    } catch (err) {
-        return [];
-    }
-}
+  try {
+    const notesBuffer = fs.readFileSync("./data.json");
+    return JSON.parse(notesBuffer.toString());
+  } catch (err) {
+    return [];
+  }
+};
 
 const saveNotes = (notes) => {
-    fs.writeFileSync("./data.json", JSON.stringify(notes))
-    console.log("SUCCESS".brightGreen);
-}
+  fs.writeFileSync("./data.json", JSON.stringify(notes));
+  console.log("SUCCESS".brightGreen);
+};
 
 const addNote = (title, body) => {
-    const allNotes = loadNotes();
+  const allNotes = loadNotes();
+
+  const position = allNotes.findIndex(
+    (note) => note.title.toLowerCase() === title.toLowerCase()
+  );
+
+  if (position === -1) {
     allNotes.push({ title, body });
     saveNotes(allNotes);
-}
+    return;
+  }
+
+  console.log("Duplicate Title. Try Again!".red);
+};
 
 module.exports = {
-    addNote
-}
+  addNote,
+};
