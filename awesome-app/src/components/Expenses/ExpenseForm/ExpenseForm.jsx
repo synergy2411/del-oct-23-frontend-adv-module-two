416a7c9b97423eb5522cdc207284091864a233d1
Sumit Khandelwal <synergy2411@outlook.com>
2023-10-13 14:21:15 +0530

    title added

--- awesome-app/src/components/Expenses/ExpenseForm/ExpenseForm.jsx
+++ awesome-app/src/components/Expenses/ExpenseForm/ExpenseForm.jsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("First");
+
+  const titleChangeHandler = (event) => setEnteredTitle(event.target.value);
+
+  const addClickHandler = (event) => {
+    event.preventDefault();
+    console.log("Title : ", enteredTitle);
+  };
   return (
     <div className="backdrop">
       <div className="dialog-box">
@@ -14,11 +24,31 @@ const ExpenseForm = () => {
                   name="title"
                   id="title"
                   placeholder=""
+                  value={enteredTitle}
+                  onChange={titleChangeHandler}
                 />
                 <label htmlFor="title">Title:</label>
               </div>
               {/* amount */}
               {/* createdAt */}
+              {/* buttons */}
+              <div className="row">
+                <div className="col-6">
+                  <div className="d-grid">
+                    <button
+                      className="btn btn-success"
+                      onClick={addClickHandler}
+                    >
+                      Add
+                    </button>
+                  </div>
+                </div>
+                <div className="col-6">
+                  <div className="d-grid">
+                    <button className="btn btn-warning">Reset</button>
+                  </div>
+                </div>
+              </div>
             </form>
           </div>
         </div>
