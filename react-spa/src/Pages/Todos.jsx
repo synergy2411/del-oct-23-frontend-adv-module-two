b473268f45383f2ffed6bfc98032bff6450de3f8
Sumit Khandelwal <synergy2411@outlook.com>
2023-10-17 13:00:44 +0530

    item deleted

--- react-spa/src/Pages/Todos.jsx
+++ react-spa/src/Pages/Todos.jsx
@@ -1,28 +1,47 @@
 import React, { useEffect, useState } from "react";
+import classes from "./Todos.module.css";
+
+const fetchTodos = async () => {
+  try {
+    const response = await fetch("http://localhost:3030/todos");
+    const todos = await response.json();
+    return todos;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 const Todos = () => {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    const fetchTodos = async () => {
-      try {
-        const response = await fetch("http://localhost:3030/todos");
-        const todos = await response.json();
-        setTodos(todos);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    fetchTodos();
+    fetchTodos().then((todos) => setTodos(todos));
   }, []);
+
+  const deleteItemHandler = async (todoId) => {
+    try {
+      await fetch(`http://localhost:3030/todos/${todoId}`, {
+        method: "DELETE",
+      });
+
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== todoId));
+
+      // fetchTodos().then(todos => setTodos(todos));
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <div className="row">
       <div className="col-6 offset-3">
         <ul className="list-group">
           {todos.map((todo) => (
-            <li className="list-group-item" key={todo.id}>
+            <li
+              className={`list-group-item mb-3 ${classes["my-list"]}`}
+              key={todo.id}
+              onDoubleClick={() => deleteItemHandler(todo.id)}
+            >
               {todo.label.toUpperCase()}
             </li>
           ))}
