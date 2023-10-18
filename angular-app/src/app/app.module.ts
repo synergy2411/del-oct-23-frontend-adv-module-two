19ade7f87a50c803ac2eb823db4f4879c56fe2cd
Sumit Khandelwal <synergy2411@outlook.com>
2023-10-18 16:21:44 +0530

    data binding added

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
@@ -9,7 +10,8 @@ import { UsersComponent } from './users/users.component';
     AppComponent, UsersComponent
   ],
   imports: [      // Module
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],      // Service
   bootstrap: [AppComponent]
--- angular-app/src/app/users/users.component.ts
+++ angular-app/src/app/users/users.component.ts
@@ -8,4 +8,20 @@ import { Component, ViewEncapsulation } from '@angular/core';
 })
 export class UsersComponent {
 
+  user = {
+    firstName: "bill",
+    lastName: "gates",
+    income: 50000,
+    isWorking: true,
+    company: "Microsoft",
+    dob: new Date("Dec 21, 1964"),
+    img: "./assets/bill.jpg",
+    votes: 120
+  }
+
+
+  onMoreInfo(company: string) {
+    alert(`USer is working with ${company}`)
+  }
+
 }
