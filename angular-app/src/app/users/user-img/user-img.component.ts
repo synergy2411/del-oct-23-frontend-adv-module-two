ba50d9a10d86e434d99047a0e7b925f7924b7f63
Sumit Khandelwal <synergy2411@outlook.com>
2023-10-18 18:17:44 +0530

    output event

--- angular-app/src/app/users/user-img/user-img.component.ts
+++ angular-app/src/app/users/user-img/user-img.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user-img',
   templateUrl: './user-img.component.html',
   styleUrls: ['./user-img.component.css']
 })
-export class UserImgComponent implements OnInit {
+export class UserImgComponent {
+  @Input("user") user: any;
 
-  constructor() { }
+  @Output() childEvent = new EventEmitter();
 
-  ngOnInit(): void {
+  btnClick() {
+    this.childEvent.emit(this.user.company);
+    // this.childEvent.emit("Apple");
   }
-
 }
