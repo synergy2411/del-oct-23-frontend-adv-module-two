3693e63a64d7027a92e806586eaba32abe90df63
Sumit Khandelwal <synergy2411@outlook.com>
2023-10-18 17:47:41 +0530

    user info added

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { UserImgComponent } from './users/user-img/user-img.component';
+import { UserInfoComponent } from './users/user-info/user-info.component';
 
 @NgModule({
   declarations: [     // Component / Directive / Pipes
-    AppComponent, UsersComponent
+    AppComponent, UsersComponent, UserImgComponent, UserInfoComponent
   ],
   imports: [      // Module
     BrowserModule,
